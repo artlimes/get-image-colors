d0a431a3b51f54b65300cc9c4e246b31d7e44984
Lorenzo Campanis <info@lcampanis.com>
2017-03-26 20:20:03 +0300

    passed count to getRgbaPalette

--- index.js
+++ index.js
@@ -10,10 +10,17 @@ const patterns = {
   svg: /svg$/i
 }
 
-function colorPalette (input, type, callback) {
-  if (typeof type === 'function') {
-    callback = type
-    type = null
+/**
+ * @param input
+ * @param {Object} options
+ * @param {String} options.type
+ * @param {String} options.count - Total number of colors to return
+ * @param callback
+ * @returns {*}
+ */
+function colorPalette (input, options, callback) {
+  if (typeof options === 'function') {
+    callback = options
   }
 
   // SVG
@@ -21,23 +28,22 @@ function colorPalette (input, type, callback) {
     if (input.match(patterns.svg)) {
       return callback(null, getSvgColors(input, { flat: true }))
     }
-  } else if (type === 'image/svg+xml') {
+  } else if (options.type === 'image/svg+xml') {
     return callback(null, getSvgColors(input, { flat: true }))
   }
 
   // PNG, GIF, JPG
-  return paletteFromBitmap(input, type, callback)
+  return paletteFromBitmap(input, options.type, callback)
 }
 
-function paletteFromBitmap (filename, type, callback) {
+function paletteFromBitmap (filename, options, callback) {
   if (!callback) {
-    callback = type
-    type = null
+    callback = options
   }
 
   getPixels(filename, type, function (err, pixels) {
     if (err) return callback(err)
-    const palette = getRgbaPalette(pixels.data, 5).map(function (rgba) {
+    const palette = getRgbaPalette(pixels.data, options.count).map(function (rgba) {
       return chroma(rgba)
     })
 
